1ad22d64937d34f16a3ebaaf64538e94233dcd05
廖上铖 <lsch2000@qq.com>
2025-01-10 15:00:39 +0800

    参数

---
+++ codes/Final project/main.m
@@ -0,0 +1,12 @@
+% 平面弹性问题分析
+clc;clear;close all;
+
+%参数设置，这里就都先设置为0
+E = 0;   %杨氏模量
+nu = 0;  %泊松比
+planeStress = true;   %平面应力还是平面应变
+radius = 0;  %洞的半径
+sideLength = 0;  %平板的边长
+traction = 0;    %load traction
+
+
