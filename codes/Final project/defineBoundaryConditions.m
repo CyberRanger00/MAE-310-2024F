c695eb6450341c27f14aed62f58bd856e85585fb
廖上铖 <lsch2000@qq.com>
2025-01-11 15:36:18 +0800

    原来是因为边界条件没有施加好，现在好了

--- codes/Final project/defineBoundaryConditions.m
+++ codes/Final project/defineBoundaryConditions.m
@@ -1,5 +1,5 @@
 % 定义边界条件
-function [dispBC, forceBC] = defineBoundaryConditions(coords, traction, radius, sideLength)
+function [dispBC, forceBC] = defineBoundaryConditions(coords, traction,radius, sideLength)
     %coords: 节点坐标矩阵
     %traction: 外加荷载大小
     %radius: 半径参数
@@ -8,16 +8,23 @@ function [dispBC, forceBC] = defineBoundaryConditions(coords, traction, radius, 
     %设置容差值tol用于判断节点位置
     tol = 1e-6;
     %找出边界节点
-    leftNodes = find(abs(coords(:, 1)) < tol);
-    bottomNodes = find(abs(coords(:, 2)) < tol);
-    rightNodes = find(abs(coords(:, 1) - sideLength) < tol);
+    xSymNodes = find(abs(coords(:, 1)) < tol);  % x=0上的对称边界节点
+    ySymNodes = find(abs(coords(:, 2)) < tol);  % y=0上的对称边界节点
+    rightNodes = find(abs(coords(:, 1) - sideLength/2) < tol);  % 右边界节点
+    topNodes = find(abs(coords(:, 2) - sideLength/2) < tol);    % 上边界节点
     %错误检查
-    if isempty(leftNodes) || isempty(bottomNodes) || isempty(rightNodes)
+    if isempty(xSymNodes) || isempty(ySymNodes) || isempty(rightNodes) || isempty(topNodes)
         error('Boundary nodes not found.');
     end
     %定义位移边界条件
-    dispBC = [leftNodes, ones(length(leftNodes), 1), zeros(length(leftNodes), 1);
-    bottomNodes, 2 * ones(length(bottomNodes), 1), zeros(length(bottomNodes), 1)];
-    %定义力边界条件:
-    forceBC = [rightNodes, ones(length(rightNodes), 1), traction * ones(length(rightNodes), 1)];
+    %x=0边界上的水平位移约束(u=0)
+    %y=0边界上的竖直位移约束(v=0)
+    dispBC = [xSymNodes, ones(length(xSymNodes), 1), zeros(length(xSymNodes), 1);
+              ySymNodes, 2*ones(length(ySymNodes), 1), zeros(length(ySymNodes), 1)];
+    
+    %施加力边界条件
+    %右边界上的水平拉力
+    %边界上的竖直拉力
+    forceBC = [rightNodes, ones(length(rightNodes), 1), traction*ones(length(rightNodes), 1);
+               topNodes, 2*ones(length(topNodes), 1), traction*ones(length(topNodes), 1)];
 end
